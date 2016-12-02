0908e0edc54dc5c6b27abdac7331db7072768158
foxsofter <foxsofter@foxsofterdeMacBook-Pro.local>
2016-12-02 18:26:28 +0800

    Initial commit

---
+++ Example/LPDControlsKit/LPDViewController.h
@@ -0,0 +1,13 @@
+//
+//  LPDViewController.h
+//  LPDControlsKit
+//
+//  Created by foxsofter on 12/02/2016.
+//  Copyright (c) 2016 foxsofter. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LPDViewController : UIViewController
+
+@end
---
+++ Example/LPDControlsKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LPDControlsKit
+//
+//  Created by foxsofter on 12/02/2016.
+//  Copyright (c) 2016 foxsofter. All rights reserved.
+//
+
+@import UIKit;
+#import "LPDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LPDAppDelegate class]));
+    }
+}
